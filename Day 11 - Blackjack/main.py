a9813c9c08acea1569393469fa45c9f023e4d070
Nolan Maddy <nolanrmaddy@gmail.com>
2022-05-04 17:24:50 -0400

    clean up

--- Day 11 - Blackjack/main.py
+++ Day 11 - Blackjack/main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
@@ -12,49 +13,58 @@ def hand_total(hand):
         total += card
     return total
 
- 
-player_hand = []
-computer_hand = []
-continue_playing = True
+keep_playing = 'y'
 
-add_card = ''
+while keep_playing == 'y':
+    player_hand = []
+    computer_hand = []
+    continue_playing = True
 
-while continue_playing:
 
-    while len(player_hand) < 2:
-        player_hand.append(card_value())
-        computer_hand.append(card_value())
+    add_card = ''
 
-    print(f"your cards are {player_hand}")
-    print(f"dealers first card {computer_hand[0]}")
-    #check computers hand
-    if hand_total(computer_hand) < 17:
-        computer_hand.append(card_value())
-    #check players hand
-    if hand_total(player_hand) > 21:
-        if 11 in player_hand:
-            player_hand[player_hand.index(11)] = 1
-        else:
-            print(f'BUST with {player_hand}')
-            continue_playing =  False
-    else:
-        add_card = input("hit? (y/n)").lower()
+    ##Better option to have setup 2 while loops. One for compture and one for player
 
-        if add_card == 'y':
+    while continue_playing:
+
+        while len(player_hand) < 2:
             player_hand.append(card_value())
-        else:
-            continue_playing = False
+            computer_hand.append(card_value())
 
-if hand_total(player_hand) > 21:
-    print('LOSER')
-elif hand_total(player_hand) > hand_total(computer_hand):
-    print(f'WINNER, \n your hand: {player_hand} \n computer hand: {computer_hand}')
-elif hand_total(player_hand) == hand_total(computer_hand):
-    print(f'DRAW \n your hand: {player_hand} \n computer hand: {computer_hand}')
-elif hand_total(computer_hand) > 21:
-    print(f'WINNER, computer BUST \n your hand: {player_hand} \n computer hand: {computer_hand}')
-else:
-    print(f"LOSER \n your hand: {player_hand} \n computer hand: {computer_hand}")
+        print(f"your cards are {player_hand}")
+        print(f"dealers first card {computer_hand[0]}")
+        #check computers hand
+        if hand_total(computer_hand) < 17:
+            computer_hand.append(card_value())
+        #check players hand
+        if hand_total(player_hand) > 21:
+            if 11 in player_hand:
+                player_hand[player_hand.index(11)] = 1
+            else:
+                print(f'BUST with {player_hand}')
+                continue_playing =  False
+        else:
+            add_card = input("hit? (y/n)").lower()
+
+            if add_card == 'y':
+                player_hand.append(card_value())
+            else:
+                continue_playing = False
+
+    if hand_total(player_hand) > 21:
+        print('LOSER')
+    elif hand_total(player_hand) > hand_total(computer_hand):
+        print(f'WINNER, \n your hand: {player_hand} \n computer hand: {computer_hand}')
+    elif hand_total(player_hand) == hand_total(computer_hand):
+        print(f'DRAW \n your hand: {player_hand} \n computer hand: {computer_hand}')
+    elif hand_total(computer_hand) > 21:
+        print(f'WINNER, computer BUST \n your hand: {player_hand} \n computer hand: {computer_hand}')
+    else:
+        print(f"LOSER \n your hand: {player_hand} \n computer hand: {computer_hand}")
+
+    keep_playing = input("would you like to play again? (y/n) ")
+    os.system('CLS')
+
 
     
     
