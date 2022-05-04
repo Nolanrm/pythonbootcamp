8d7942e0acc3beed9405040d7fa0e496e0cc513e
Nolan Maddy <nolanrmaddy@gmail.com>
2022-05-04 17:15:44 -0400

    finished blackjack

--- Day 11 - Blackjack/main.py
+++ Day 11 - Blackjack/main.py
@@ -1,8 +1,10 @@
 import random
 
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
 def card_value():
-    card_value = random.randint(1,11)
-    return card_value
+    card_value = random.randint(1,12)
+    return cards[card_value]
 
 def hand_total(hand):
     total = 0
@@ -22,14 +24,20 @@ while continue_playing:
     while len(player_hand) < 2:
         player_hand.append(card_value())
         computer_hand.append(card_value())
-    
-    if hand_total(player_hand) > 21:
-        print(f'BUST with {player_hand}')
-        continue_playing =  False
-    else:
-        print(f"your cards are {player_hand}")
-        print(f"dealers first card {computer_hand[0]}")
 
+    print(f"your cards are {player_hand}")
+    print(f"dealers first card {computer_hand[0]}")
+    #check computers hand
+    if hand_total(computer_hand) < 17:
+        computer_hand.append(card_value())
+    #check players hand
+    if hand_total(player_hand) > 21:
+        if 11 in player_hand:
+            player_hand[player_hand.index(11)] = 1
+        else:
+            print(f'BUST with {player_hand}')
+            continue_playing =  False
+    else:
         add_card = input("hit? (y/n)").lower()
 
         if add_card == 'y':
@@ -37,4 +45,19 @@ while continue_playing:
         else:
             continue_playing = False
 
+if hand_total(player_hand) > 21:
+    print('LOSER')
+elif hand_total(player_hand) > hand_total(computer_hand):
+    print(f'WINNER, \n your hand: {player_hand} \n computer hand: {computer_hand}')
+elif hand_total(player_hand) == hand_total(computer_hand):
+    print(f'DRAW \n your hand: {player_hand} \n computer hand: {computer_hand}')
+elif hand_total(computer_hand) > 21:
+    print(f'WINNER, computer BUST \n your hand: {player_hand} \n computer hand: {computer_hand}')
+else:
+    print(f"LOSER \n your hand: {player_hand} \n computer hand: {computer_hand}")
+
+    
+    
+
+        
 
