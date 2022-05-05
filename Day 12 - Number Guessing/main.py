dd5575dbf7ce33981193b6dbd3fbe6d28157700e
Nolan Maddy <nolanrmaddy@gmail.com>
2022-05-04 20:24:10 -0400

    finished number guessing

--- Day 12 - Number Guessing/main.py
+++ Day 12 - Number Guessing/main.py
@@ -27,6 +27,8 @@ def check_guess(guess,remaining_guesses):
         CORRECT_ANSWER = True
         return [remaining_guesses, CORRECT_ANSWER]
 
+
+print("Welcome to the number guessing game \nI am thinking of a number between 1 and 100.")
 difficulty = input("choose a difficulty, type 'hard' or 'easy': ").lower()
 
 if difficulty == 'hard':
@@ -40,6 +42,11 @@ while remaining_guesses > 0 and CORRECT_ANSWER == False:
     response = check_guess(guess, remaining_guesses)
     remaining_guesses = response[0]
     CORRECT_ANSWER = response[1]
-    print(f'you have {remaining_guesses} guesses remaining')
+    if remaining_guesses > 0 and CORRECT_ANSWER == True:
+        print(f'Winner correct number was {guess}')
+    elif remaining_guesses == 0 and CORRECT_ANSWER == False:
+        print(f'Out of guesses, correct answer was {FINAL_ANSWER}')
+    else:
+        print(f'you have {remaining_guesses} guesses remaining')
 
 
