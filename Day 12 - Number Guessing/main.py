556f71b45394dbdc77f8a41a135d82d97d258ff5
Nolan Maddy <nolanrmaddy@gmail.com>
2022-05-04 20:15:41 -0400

    number guessing game

---
+++ Day 12 - Number Guessing/main.py
@@ -0,0 +1,45 @@
+#learn scope
+
+
+#There is no block scope
+
+import random
+
+FINAL_ANSWER = random.randint(1,100)
+CORRECT_ANSWER = False
+
+
+
+def check_guess(guess,remaining_guesses):
+    if guess > FINAL_ANSWER:
+        remaining_guesses -= 1
+        CORRECT_ANSWER = False
+        print("Guess is too high")
+        return  [remaining_guesses, CORRECT_ANSWER]
+    elif guess < FINAL_ANSWER:
+        remaining_guesses -= 1
+        CORRECT_ANSWER = False
+        print("Guess is too low")
+        return [remaining_guesses, CORRECT_ANSWER]
+    if guess == FINAL_ANSWER:
+        print("Correct!")
+        remaining_guesses = 0
+        CORRECT_ANSWER = True
+        return [remaining_guesses, CORRECT_ANSWER]
+
+difficulty = input("choose a difficulty, type 'hard' or 'easy': ").lower()
+
+if difficulty == 'hard':
+    remaining_guesses = 5
+else:
+    remaining_guesses = 10
+
+while remaining_guesses > 0 and CORRECT_ANSWER == False:
+    guess = int(input("Make a guess: "))
+
+    response = check_guess(guess, remaining_guesses)
+    remaining_guesses = response[0]
+    CORRECT_ANSWER = response[1]
+    print(f'you have {remaining_guesses} guesses remaining')
+
+
