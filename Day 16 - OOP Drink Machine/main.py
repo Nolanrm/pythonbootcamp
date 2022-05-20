16c42edac4e282d73e1f49a04a74ce29bf5db493
Nolan Maddy <nolanrmaddy@gmail.com>
2022-05-20 18:04:30 -0400

    started day 16

---
+++ Day 16 - OOP Drink Machine/main.py
@@ -0,0 +1,31 @@
+# import turtle
+
+# timmy = turtle.Turtle()
+# timmy.shape('turtle')
+# timmy.color('#6495ED')
+# timmy.forward(20)
+# timmy.right(90)
+# timmy.forward(100)
+
+# my_screen = turtle.Screen()
+# my_screen.exitonclick()
+
+
+# import prettytable
+
+# table = prettytable.PrettyTable()
+
+# table.add_column('pokemon name',['pikachu','squirtle','charmander'])
+# table.add_column('type',['electric','water','fire'])
+# table.align = 'l'
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+machine_on = True
+
+while machine_on == True:
+    pass
+
