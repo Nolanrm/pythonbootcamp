c53670038fd95cbac409337f6ee4146137ce4d8e
Nolan Maddy <nolanrmaddy@gmail.com>
2022-05-21 08:43:09 -0400

    finished oop coffee machine

--- Day 16 - OOP Drink Machine/main.py
+++ Day 16 - OOP Drink Machine/main.py
@@ -23,9 +23,31 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
 
 machine_on = True
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+my_menu = Menu()
 
 while machine_on == True:
-    pass
+    
+    customer_request = input(f'what would you like to order {my_menu.get_items()}?').lower()
+
+    if customer_request == 'off':
+        os.system('CLS')
+        machine_on = False
+    elif customer_request == 'report':
+        os.system('CLS')
+        print(coffee_machine.report())
+    else:
+        os.system('CLS')
+        drink = my_menu.find_drink(customer_request)
+        if coffee_machine.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_machine.make_coffee(drink)
+            else:
+                pass
+
+
 
